900382e30aab88f926484acb49ab067ecc512340
Andrae Genesis Valin <109593286+Andrae009@users.noreply.github.com>
2025-02-21 21:41:02 +0800

    Javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for page to load before animations
+document.addEventListener("DOMContentLoaded", function() {
+    let subtitle = document.querySelector("p");
+    subtitle.style.opacity = "1";
+});
