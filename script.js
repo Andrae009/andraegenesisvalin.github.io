a8e2bc5a1f43c1ab5773cbb6f1a3278fbce0e246
Andrae Genesis Valin <109593286+Andrae009@users.noreply.github.com>
2025-02-21 23:48:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const projects = document.querySelectorAll(".project-card");
 
-    projects.forEach(card => {
-        card.addEventListener("mouseover", function() {
-            this.style.transform = "scale(1.1)";
-        });
-        card.addEventListener("mouseout", function() {
-            this.style.transform = "scale(1)";
-        });
-    });
-});
